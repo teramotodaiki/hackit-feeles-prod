b8a72d5887cf5cd31bf88a9a12369d85d45196ce
teramotodaiki <i101323@gmail.com>
2016-11-13 12:07:33 +0900

    seeding

---
+++ database/seeds/ContentsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Content;
+
+class ContentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Content::create([
+            'user_id' => 1,
+            'src' => 'https://teramotodaiki.github.io/h4p/dist/',
+            'title' => 'Feeles Example',
+            'description' => '......Here is a description......',
+            'thumbnail' => 'http://bit.ly/2eTq3oR',
+        ]);
+    }
+}
