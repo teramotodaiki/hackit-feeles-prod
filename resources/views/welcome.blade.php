2a675bf0a530207778cb024763c9c63b2fa14204
teramotodaiki <i101323@gmail.com>
2016-11-14 14:41:46 +0900

    links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,11 +79,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/contents">Works</a>
+                    @if (Auth::check() && Auth::user()->is_admin)
+                    <a href="/users">Member</a>
+                    @endif
                 </div>
             </div>
         </div>
