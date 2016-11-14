44c2c215032ffffaddd0fc0fb77ee884cdde2e85
teramotodaiki <i101323@gmail.com>
2016-11-14 14:01:12 +0900

    view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container user_list">
+    @foreach ($users as $user)
+    <div class="row user_item{{ $loop->last ? ' user_item-last' : '' }}">
+        <div class="col-xs-1">
+            @if ($user->is_admin)
+            <span class="glyphicon glyphicon-king"></span>
+            @endif
+        </div>
+        <div class="col-xs-5">
+            {{ $user->name }}
+        </div>
+        <div class="col-xs-3">
+            <span>{{ $user->userid }}</span>
+        </div>
+        <div class="col-xs-3">
+            <span>{{ $user->created_at }}</span>
+        </div>
+    </div>
+    @endforeach
+</div>
+<div class="text-center">
+{{ $users->links() }}
+</div>
 @endsection
