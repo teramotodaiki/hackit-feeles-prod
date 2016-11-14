e8b91eb8ca1980efdcbb6d3a6cc6e34998c9fc59
teramotodaiki <i101323@gmail.com>
2016-11-14 20:23:12 +0900

    view fix

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -3,26 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-2">
+        <div class="col-md-3 col-md-offset-0 col-sm-6 col-sm-offset-3">
             <div class="panel panel-default">
 
                 <div class="panel-body">
-                    @include('components.icon', ['src' => $user->thumbnail])
-                    <h3>{{ $user->name }}</h3>
-                    <h5>{{ '@' . $user->userid }}</h5>
+                    <div class="row">
+                        <div class="col-md-12 col-xs-6">
+                            @include('components.icon', ['src' => $user->thumbnail])
+                        </div>
+                        <div class="col-md-12 col-xs-6">
+                            <h3>{{ $user->name }}</h3>
+                            <h5>{{ '@' . $user->userid }}</h5>
+                        </div>
+                    </div>
                 </div>
 
                 @if (Auth::user()->id === $user->id)
                 <div class="panel-footer">
                     <a href="{{ url("/users/{$user->id}/edit") }}" type="button" class="btn btn-primary btn-block">
-                        Update
+                        Edit
                     </a>
                 </div>
                 @endif
 
             </div>
         </div>
-        <div class="col-md-10 container">
+        <div class="col-md-9 col-sm-12 container">
             <div class="row content_list">
             @each('contents.item', $contents, 'content')
             </div>
