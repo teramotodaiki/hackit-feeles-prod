7bf1a2eb39a97554cb79e5038619e1e05778305e
teramotodaiki <i101323@gmail.com>
2016-11-14 11:45:26 +0900

    userid instead of email

---
+++ database/migrations/2016_11_14_020108_add_students_column.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStudentsColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table)
+        {
+            $table->boolean('is_admin')->default(false);
+            $table->string('userid')->unique();
+            $table->boolean('is_male')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->softDeletes();
+
+            $table->dropUnique(['email']);
+            $table->string('email')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table)
+        {
+            $table->dropUnique(['userid']);
+            $table->dropColumn([
+                'is_admin',
+                'userid',
+                'is_male',
+                'birthday',
+                'thumbnail',
+                'deleted_at'
+            ]);
+        });
+    }
+}
