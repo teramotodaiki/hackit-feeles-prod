59e1e4077befb178914d962a965bb568054efca8
teramotodaiki <i101323@gmail.com>
2016-11-14 20:06:03 +0900

    responsive icon

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -7,9 +7,7 @@
             <div class="panel panel-default">
 
                 <div class="panel-body">
-                    @if ($user->thumbnail)
-                    <img src="{{ $user->thumbnail }}" alt="" />
-                    @endif
+                    @include('components.icon', ['src' => $user->thumbnail])
                     <h3>{{ $user->name }}</h3>
                     <h5>{{ '@' . $user->userid }}</h5>
                 </div>
