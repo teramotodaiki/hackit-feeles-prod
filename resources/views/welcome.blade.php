b93ef21537164a4da9011e649916bd0597b5e050
teramotodaiki <i101323@gmail.com>
2016-11-15 19:40:24 +0900

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,10 @@
         </style>
     </head>
     <body>
+        @if(session('message'))
+        <div style="color: red">{{ session('message') }}</div>
+        @endif
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
