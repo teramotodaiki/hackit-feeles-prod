ae9230b8f0ca05a3877cacb9926c9673181c5477
teramotodaiki <i101323@gmail.com>
2016-11-15 22:07:34 +0900

    template

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use DB;
+use Illuminate\Http\Request;
+use App\Invitation;
+
+class AdminController extends Controller
+{
+    // 
+}
