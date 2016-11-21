8c622b1fca72dd8e59de8aa28f62bc325bd0bac9
teramotodaiki <i101323@gmail.com>
2016-11-21 14:44:35 +0900

    Content list as state

---
+++ resources/views/contents/item-list.blade.php
@@ -0,0 +1,21 @@
+<div class="content_list content_toggle" data-content="{{ $content->id }}">
+    <div class="content_thumbnail" style="background-image: url({{ $content->thumbnail or '/image/thumbnail-default.png' }})">
+        <div>
+        {{
+            $content->description or trans('content.no_descriptions')
+        }}
+        </div>
+    </div>
+    <div class="content_article">
+        <h3>{{ $content->title }}</h3>
+        @if (Auth::check() && $content->user_id === Auth::user()->id)
+        <a href="/contents/{{ $content->id }}/edit" class="btn btn-primary btn-sm">
+            @lang('form.edit')
+        </a>
+        @else
+        <a href="/users/{{ $content->user_id }}" class="btn btn-link" title="{{ $content->user->name }}">
+            {{ $content->user->name }}
+        </a>
+        @endif
+    </div>
+</div>
--- resources/views/contents/item.blade.php
+++ resources/views/contents/item.blade.php
@@ -1,23 +1,8 @@
-<div class="col-md-3 col-sm-4 content_item">
-    <div onclick="location.assign('/contents/{{ $content->id }}')">
-        <div class="content_thumbnail" style="background-image: url({{ $content->thumbnail or '/image/thumbnail-default.png' }})">
-            <div>
-            {{
-                $content->description or trans('content.no_descriptions')
-            }}
-            </div>
-        </div>
-        <div class="content_article">
-            <h3>{{ $content->title }}</h3>
-            @if (Auth::check() && $content->user_id === Auth::user()->id)
-            <a href="/contents/{{ $content->id }}/edit" class="btn btn-primary btn-sm">
-                @lang('form.edit')
-            </a>
-            @else
-            <a href="/users/{{ $content->user_id }}" class="btn btn-link" title="{{ $content->user->name }}">
-                {{ $content->user->name }}
-            </a>
-            @endif
-        </div>
-    </div>
+<div class="col-md-3 col-sm-4 content_item content_item-list">
+    @include('contents.item-list', [
+        'content' => $content,
+    ])
+    @include('contents.item-playing', [
+        'content' => $content,
+    ])
 </div>
