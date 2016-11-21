6ce4c7647fd78f6232ae9c841da6128a34428bb3
teramotodaiki <i101323@gmail.com>
2016-11-21 15:17:41 +0900

    comment out Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,9 +13,10 @@ require('./bootstrap');
  * the application, or feel free to tweak this setup for your needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
+//
+// const app = new Vue({
+//     el: '#app'
+// });
 
-const app = new Vue({
-    el: '#app'
-});
 require('./content');
