6be0c51fce25880eababf945df4375a22a639600
teramotodaiki <i101323@gmail.com>
2016-11-21 14:45:55 +0900

    Switch state with click event

---
+++ resources/assets/js/content.js
@@ -0,0 +1,22 @@
+const classes = 'col-md-3 col-sm-4 col-xs-12 content_item-list content_item-playing';
+
+$('.content_item').on('click', (event) => {
+  const $item = $(event.currentTarget);
+  if (!$item.hasClass('content_item-list')) {
+    return;
+  }
+
+  const $iframe = $item.find('iframe');
+  if (!$iframe.attr('src')) {
+    $iframe.attr('src', $iframe.data('src'));
+  }
+
+  $item.toggleClass(classes);
+});
+
+$('.content_close').on('click', (event) => {
+  const $item = $(event.target).parents('.content_item-playing');
+  $item.toggleClass(classes);
+
+  event.stopPropagation();
+});
