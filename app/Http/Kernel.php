51bc37568e512e3385a9a591c2f1b95414e71dcc
teramotodaiki <i101323@gmail.com>
2016-11-27 14:13:03 +0900

    Raw data uploader

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,5 +54,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\CheckAdmin::class,
         'uploader' => \App\Http\Middleware\FileUploader::class,
+        'uploader.raw' => \App\Http\Middleware\RawUploader::class,
     ];
 }
