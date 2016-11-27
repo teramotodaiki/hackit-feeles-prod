407e0c30b222372e65c3e77e180f4fff03df639d
teramotodaiki <i101323@gmail.com>
2016-11-27 14:18:15 +0900

    Upload view

---
+++ resources/assets/js/components/uploadFromFeeles.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="content_loading">
+            <span class="glyphicon glyphicon-refresh"></span>
+        </div>
+        <form id="upload-from-feeles" class="hidden" v-bind:action="'/contents/' + contentId + '/popout'" method="POST">
+            <input type="hidden" name="_token" value="" />
+            <input type="hidden" name="content" value="" />
+        </form>
+    </div>
+</template>
+<script>
+    export default {
+
+        props: ['contentId'],
+
+        mounted() {
+            const $form = $('#upload-from-feeles');
+            $form.find('input[name="_token"]').val(Laravel.csrfToken);
+
+            const channel = new MessageChannel();
+            channel.port1.onmessage = (event) => {
+                $form.find('input[name="content"]').val(event.data);
+                $form.submit();
+            };
+
+            parent.postMessage('', '*', [channel.port2]);
+        }
+    }
+</script>
