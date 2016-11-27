e59281dc77c53e4d05a1387931fa30a7e1d39418
teramotodaiki <i101323@gmail.com>
2016-11-27 14:18:34 +0900

    Popout view

--- resources/views/contents/uploadFromFeeles.blade.php
+++ resources/views/contents/uploadFromFeeles.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
+@if(session('status'))
+<div class="text-center">
+    Successfully Uploaded!!
+</div>
+@else
+<upload-from-feeles content-id="{{ $content->id }}"></upload-from-feeles>
+@endif
+
 @endsection
