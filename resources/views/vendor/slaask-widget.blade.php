78ca87f693946af2eb829d35cb129a530c158df0
teramotodaiki <i101323@gmail.com>
2016-12-13 18:24:23 +0900

    remove debug log

--- resources/views/vendor/slaask-widget.blade.php
+++ resources/views/vendor/slaask-widget.blade.php
@@ -1,7 +1,6 @@
 @if(env('SLAASK_WIDGET_KEY'))
 <script src='https://cdn.slaask.com/chat.js'></script>
 <script>
-    console.log(_slaask);
     _slaask.init('{{ env('SLAASK_WIDGET_KEY') }}');
 </script>
 @endif
