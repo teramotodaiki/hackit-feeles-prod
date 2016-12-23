3cb041c37dd3897e22cd8bc3f1b4aaadd0ba7e87
teramotodaiki <i101323@gmail.com>
2016-12-23 14:19:15 +0900

    UI

--- resources/lang/ja/content.php
+++ resources/lang/ja/content.php
@@ -2,4 +2,11 @@
 
 return [
     'no_descriptions' => '説明文なし',
+    'sort' => '並び順',
+    'title_asc' => '辞書順',
+    'title_desc' => '逆順',
+    'created_at_asc' => '古い順',
+    'created_at_desc' => '新しい順',
+    'updated_at_asc' => '更新された順',
+    'updated_at_desc' => '更新されていない順',
 ];
